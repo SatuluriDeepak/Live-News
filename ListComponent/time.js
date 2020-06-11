0e4b4d528ec3ab80ad03ff6f21468df2c6ab7384
SatuluriDeepak <65010016+SatuluriDeepak@users.noreply.github.com>
2020-06-11 12:52:05 +0530

    Add files via upload
    Lists

---
+++ ListComponent/time.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import {ListItem,Right,Left,Thumbnail,Body,Text,Button, View} from 'native-base';
+import moment from 'moment';
+export default class Time extends Component{
+    constructor(props){
+        super(props);
+        this.date=props.time;
+        
+    }
+    render(){
+        const time=moment(this.date || moment.now()).fromNow();
+        return( 
+        <Text note style={{marginHorizontal:13}}>{time}</Text>
+        );
+    } 
+}
